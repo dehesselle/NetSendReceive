4e8dc6d796547f63c07ab8011b7ad39a851f8127
René de Hesselle <dehesselle@web.de>
2018-11-11 17:11:06 +0100

    Remove dead code

--- TcpClient.h
+++ TcpClient.h
@@ -11,15 +11,6 @@ class TcpClient : public TcpConnection
     Q_OBJECT
 public:
     TcpClient(QObject *parent = nullptr);
-/*
-    bool connectTo(const QString &host, const quint16 &port);
-
-    void sendMessage(const QString &message);
-
-private:
-    Connection* m_connection = nullptr;
-
-    */
 };
 
 #endif // TCPCLIENT_H
